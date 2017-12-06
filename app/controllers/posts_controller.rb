30db179cb68a2577170b4ad4b241255a2f88f978
Matthew Harris <mharr171.z@gmail.com>
2017-12-06 02:41:03 -0800

    assignment-17: complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+
+    @posts.each_with_index do |p,i|
+      p.title = "Spam" if i % 5 == 0
+    end
   end
 
   def show
