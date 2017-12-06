d3a884126dfd65171d06f0a6b3b5f199d36fc555
Matthew Harris <mharr171.z@gmail.com>
2017-12-06 01:36:29 -0800

    assignment-16: complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,14 @@ require 'random_data'
     body:   RandomData.random_paragraph
   )
 end
+
+uniq_title = "Unique Title"
+uniq_body = "Unique body"
+Post.find_or_create_by!(
+  title: uniq_title,
+  body: uniq_body
+)
+
 posts = Post.all
 
 100.times do
@@ -14,6 +22,14 @@ posts = Post.all
     body: RandomData.random_paragraph
   )
 end
+uniq_com_body = "Unique comment body"
+Comment.find_or_create_by!(
+  post: Post.find_or_create_by!(
+    title: uniq_title,
+    body: uniq_body
+  ),
+  body: uniq_com_body
+)
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
