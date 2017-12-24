35077c382a77077ad1486c02d10e7435043c55e9
Matthew Harris <mharr171.z@gmail.com>
2017-12-24 15:56:34 -0800

    assignment-23: complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase if email.present? }
 
+  before_save :format_name
+
   validates :name,
             presence: true ,
             length: { minimum: 1, maximum: 100 }
@@ -20,4 +22,15 @@ class User < ApplicationRecord
             length: { minimum: 3, maximum: 254 }
 
   has_secure_password
+
+  def format_name
+    if name
+      n_arr = []
+      name.split.each do |n|
+        n_arr << n.capitalize
+      end
+      self.name = n_arr.join(' ')
+    end
+  end
+
 end
